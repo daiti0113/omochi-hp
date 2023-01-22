44d83ad5f2ad632f62da71f2a0a4c7200d9b132c
Daichi0113 <35861846+daiti0113@users.noreply.github.com>
2023-01-22 17:10:35 +0900

    Initial commit

---
+++ pages/portfolio.tsx
@@ -0,0 +1,21 @@
+import Container from '../components/container'
+import Layout from '../components/layout'
+import Head from 'next/head'
+
+export default function Index() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Portfolio</title>
+        </Head>
+        <Container>
+          <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+            Portfolio.
+          </h1>
+          <p>これはテストページです。</p>
+        </Container>
+      </Layout>
+    </>
+  )
+}
