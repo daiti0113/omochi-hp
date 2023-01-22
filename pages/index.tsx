f35e1063d5406327861042025922a927e48241bd
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-22 22:03:40 +0900

    実績値のセクションを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,47 @@ export default function Index() {
           </div>
         </section>
         {/* <!-- text - end --> */}
+        <section>
+          <div className="bg-white py-6 sm:py-8 lg:py-12">
+            <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
+              <div className="grid md:grid-cols-2 gap-8 lg:gap-12">
+                <div>
+                  <div className="h-64 md:h-auto bg-gray-100 overflow-hidden rounded-lg shadow-lg">
+                    <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750" loading="lazy" alt="Photo by Martin Sanchez" className="w-full h-full object-cover object-center" />
+                  </div>
+                </div>
+                <div>
+                  <h1 className="text-gray-800 text-2xl sm:text-3xl font-bold text-center md:text-left mb-4 md:mb-6">こんなに作ったんだよ。<br />すごいでしょ。</h1>
+                  <p className="text-gray-500 text-center md:text-start text-base mb-6 md:mb-8 break-keep">
+                    美容室とかカフェとかたくさん回って、丁寧にヒアリングしてたくさん作ったんだよ。頼まれたらすぐ作っちゃうから依頼してね。でーじ上等な一眼レフも買ったから、素材がないときも気軽に相談していいからね。
+                  </p>
+                  <div className="grid grid-cols-1 sm:grid-cols-3 px-10 sm:px-0 gap-4">
+                    {/* <!-- feature - start --> */}
+                    <div className="flex flex-col items-center sm:items-start">
+                      <span className="text-gray-600 text-6xl lg:text-6xl font-semibold mb-2">17</span>
+                      <p className="text-gray-600 font-semibold mb-2">サイト数</p>
+                    </div>
+                    {/* <!-- feature - end --> */}
+
+                    {/* <!-- feature - start --> */}
+                    <div className="flex flex-col items-center sm:items-start">
+                      <span className="text-gray-600 text-6xl lg:text-6xl font-semibold mb-2">23</span>
+                      <p className="text-gray-600 font-semibold mb-2">ページ数</p>
+                    </div>
+                    {/* <!-- feature - end --> */}
+
+                    {/* <!-- feature - start --> */}
+                    <div className="flex flex-col items-center sm:items-start">
+                      <span className="text-gray-600 text-6xl lg:text-6xl font-semibold mb-2">360</span>
+                      <p className="text-gray-600 text-base font-semibold mb-2">アクセス数</p>
+                    </div>
+                    {/* <!-- feature - end --> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
   )
 }
