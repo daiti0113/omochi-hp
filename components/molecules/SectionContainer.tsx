cc0bc505e2db74173b2a64d214961a17d9032e22
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-27 22:56:23 +0900

    SectionContainerのmargin-bottomを調整

--- components/molecules/SectionContainer.tsx
+++ components/molecules/SectionContainer.tsx
@@ -4,7 +4,7 @@ type SectionContainerProps = {
 
 export const SectionContainer = ({children}: SectionContainerProps) => {
     return (
-        <section className="px-10 md:px-20 lg:px-48">
+        <section className="px-10 md:px-20 lg:px-48 mb-16 sm:mb-20 md:mb-24">
             {children}
         </section>
     )
