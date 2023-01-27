8d59f906330f98339493c0bc4d43e73fdb577d51
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-27 23:06:55 +0900

    Heroのスタイルを調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,12 @@ export default function Index({ allPosts }: Props) {
         </Head>
         {/* TODO: Heroコンポーネントに移動する */}
         <SectionContainer>
-          <div className="xl:w-5/12 flex flex-col justify-center items-center lg:items-start sm:text-center lg:text-left lg:py-12 xl:py-24">
-            <h1 className="text-black-800 text-6x sm:text-5xl md:text-6xl font-bold mb-8 md:mb-12">ホームページを<br />作成するよ</h1>
-            <form className="w-full md:max-w-md flex gap-2">
+          <div className="xl:w-5/12 flex flex-col justify-center items-center md:items-start sm:text-center md:text-left lg:py-12 xl:py-24">
+            <h1 className="text-black text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold mb-8 md:mb-12 md:w-96">
+              <span className="inline-block leading-tight">ホームページを</span>
+              <span className="inline-block leading-tight">作成するよ</span>
+            </h1>
+            <form className="flex xs:self-start">
               <button className="inline-block bg-black hover:bg-black-600 active:bg-black-700 focus-visible:ring ring-black-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">Sign up</button>
             </form>
           </div>
