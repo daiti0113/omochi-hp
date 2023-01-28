dfe6e82ef776a5de126d18348c23d9df8a852927
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-28 23:26:47 +0900

    制作事例セクションのレスポンシブ対応

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const MoreStories = ({ posts }: Props) => {
   return (
-    <section className="w-full">
-      <h2 className="pl-48 mb-8 text-center md:text-start text-2xl sm:text-3xl font-bold tracking-tighter leading-tight">
+    <section className="w-full mt-16 sm:mt-20 md:mt-24">
+      <h2 className="px-10 md:px-20 lg:px-48 mb-8 text-center md:text-start text-2xl sm:text-3xl font-bold tracking-tighter leading-tight">
         制作事例
       </h2>
       <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
-        <div className="flex mx-48 gap-x-10">
+        <div className="flex gap-x-10 mx-10 md:mx-20 lg:mx-48 ">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
