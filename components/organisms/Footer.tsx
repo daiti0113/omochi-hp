6d4f0aea3c668804a8c4ae20675950468197fd1f
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-29 00:30:09 +0900

    Footerのレスポンシブ対応

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <footer className="bg-black mt-32 px-10 md:px-20 lg:px-48 py-12">
-      <div className="flex">
-        <div className="flex-none w-60">
-          <Link href="/" className="text-white hover:text-gray-300 text-3xl lg:text-[2.5rem] lg:text-left h-fit w-fit">
+    <footer className="bg-black mt-8 md:mt-32 px-10 md:px-20 lg:px-48 py-12">
+      <div className="sm:flex">
+        <div className="flex-none mr-10 sm:w-60">
+          <Link href="/" className="text-white hover:text-gray-300 text-2xl sm:text-3xl lg:text-[2.5rem] lg:text-left h-fit w-fit">
             Omochi
           </Link>
         </div>
-        <div className="flex-1 flex flex-col">
-          <Link href="/" className="text-white hover:text-gray-500 transition duration-100 w-fit">HOME</Link>
-          <Link href="/portfolio" className="text-white hover:text-gray-500 transition duration-100 w-fit">PORTFOLIO</Link>
-          <Link href="/pricing" className="text-white hover:text-gray-500 transition duration-100 w-fit">PRICING</Link>
-          <Link href="/contact-us" className="text-white hover:text-gray-500 transition duration-100 w-fit">CONTACT US</Link>
+        <div className="flex-1 flex flex-col gap-y-2 mt-8 sm:mt-0">
+          <Link href="/" className="text-white hover:text-gray-500 text-sm transition duration-100 w-fit">HOME</Link>
+          <Link href="/portfolio" className="text-white hover:text-gray-500 text-sm transition duration-100 w-fit">PORTFOLIO</Link>
+          <Link href="/pricing" className="text-white hover:text-gray-500 text-sm transition duration-100 w-fit">PRICING</Link>
+          <Link href="/contact-us" className="text-white hover:text-gray-500 text-sm transition duration-100 w-fit">CONTACT US</Link>
         </div>
       </div>
     </footer>
