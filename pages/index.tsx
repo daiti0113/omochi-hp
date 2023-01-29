d283e6e7923f4cbfbb9f7298dc150035afad149d
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-29 22:15:07 +0900

    Lint対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import {SITE_NAME} from "../lib/constants"
 import {getAllPosts} from "../lib/api"
 import MoreStories from "../components/more-stories"
 import Post from "../interfaces/post"
-import { Hero } from "../components/organisms/Hero"
-import { Message } from "../components/organisms/Message"
-import { Stats } from "../components/organisms/Stats"
+import {Hero} from "../components/organisms/Hero"
+import {Message} from "../components/organisms/Message"
+import {Stats} from "../components/organisms/Stats"
 
 type Props = {
   allPosts: Post[]
