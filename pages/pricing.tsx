d99a1588828b117e57ed3dff866e6eedcc7dc39e
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-30 23:41:26 +0900

    コンポーネントの分け方をリファクタ

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/layout"
 import Head from "next/head"
-import {Pricing} from "../components/organisms/Pricing"
+import {Headline} from "../components/atoms/Headline"
+import {PricingTable} from "../components/organisms/PricingTable"
 
 export default function Index() {
     return (
@@ -9,7 +10,16 @@ export default function Index() {
                 <Head>
                     <title>Pricing</title>
                 </Head>
-                <Pricing />
+                <div className="bg-white py-6 sm:py-8 lg:py-12">
+                    <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
+                        <Headline>料金プラン</Headline>
+                        <p className="max-w-screen-md text-gray-500 text-sm md:text-base mt-8 text-center mx-auto">
+                            <span>初期費用として制作費を頂きホームページが完成してから、</span>
+                            <span>運用代行費としての月額料金になるシンプルなプランです。</span>
+                        </p>
+                    </div>
+                </div>
+                <PricingTable />
             </Layout>
         </>
     )
