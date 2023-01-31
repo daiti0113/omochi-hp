c7e411e99df77f68d73e11622ae697b5268a0ed3
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-01 01:02:50 +0900

    制作事例の一例のコンポーネントを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import Layout from "../components/layout"
 import Head from "next/head"
 import {SITE_NAME} from "../lib/constants"
 import {getAllPosts} from "../lib/api"
-import MoreStories from "../components/more-stories"
 import Post from "../interfaces/post"
 import {Hero} from "../components/organisms/Hero"
 import {Message} from "../components/organisms/Message"
 import {Stats} from "../components/organisms/Stats"
+import {MorePosts} from "../components/organisms/MorePosts"
 
 type Props = {
   allPosts: Post[]
@@ -22,7 +22,7 @@ export default function Index({allPosts}: Props) {
             <Hero />
             <Message />
             <Stats />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            {morePosts.length > 0 && <MorePosts posts={morePosts} />}
         </Layout>
     )
 }
