909bb3b925d8e7323cf5abece6e4f45e2401dea1
daiti0113 <stennis.ds.0113@gmail.com>
2023-01-31 23:24:54 +0900

    お問い合わせのリンクを追加

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -4,6 +4,7 @@ import {Headline} from "../components/atoms/Headline"
 import {PricingTable} from "../components/organisms/PricingTable"
 import {SectionContainer} from "../components/molecules/SectionContainer"
 import {OptionTable} from "../components/organisms/OptionTable"
+import { CONTACT_URL } from "../lib/constants"
 
 // eslint-disable-next-line max-lines-per-function
 export default function Index() {
@@ -17,11 +18,14 @@ export default function Index() {
                     <div className="bg-white py-6 sm:py-8 lg:py-12">
                         <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
                             <Headline>料金プラン</Headline>
-                            <p className="max-w-screen-md text-gray-500 text-sm md:text-base mt-8 text-center mx-auto">
-                                <span>初期費用として制作費を頂きホームページが完成してから、</span>
-                                <span>運用代行費としての月額料金になるシンプルなプランです。</span>
+                            <p className="max-w-screen-md text-gray-500 text-sm md:text-base mt-8 text-center mx-auto break-keep">
+                                <span>お客様の現状をヒアリングし、要望にあったプランを提案します。</span>
+                                <span>ぜひまずはお気軽にお問い合わせください！</span>
                             </p>
                         </div>
+                    </div>
+                    <div className="flex mt-4 mb-10">
+                        <a href={CONTACT_URL} className="block mx-auto bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3 md:w-full md:max-w-xs">お問い合わせ</a>
                     </div>
                     <PricingTable />
                 </SectionContainer>
@@ -35,6 +39,9 @@ export default function Index() {
                         </div>
                     </div>
                     <OptionTable />
+                    <div className="flex mt-4 mb-10">
+                        <a href={CONTACT_URL} className="block mx-auto bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3 md:w-full md:max-w-xs">お問い合わせ</a>
+                    </div>
                 </SectionContainer>
             </Layout>
         </>
