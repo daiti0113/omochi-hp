017443aa2691a7046eb293c2508f1450c2c7a911
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-08 01:48:29 +0900

    料金プランの名称を日本語に変更

--- components/organisms/PricingTable.tsx
+++ components/organisms/PricingTable.tsx
@@ -7,7 +7,7 @@ export const PricingTable = () => {
             {/* <!-- plan - start --> */}
             <div className="flex flex-col space-y-4">
                 <div className="flex flex-col border rounded-lg gap-4 p-4 pt-6">
-                    <h3 className="text-gray-800 text-2xl font-semibold text-center">START</h3>
+                    <h3 className="text-gray-800 text-2xl font-semibold text-center">スタンダード</h3>
                     <div className="flex justify-center items-end gap-1">
                         <span className="text-4xl text-gray-800 font-bold">¥6,000</span>
                         <span className="text-gray-500">/ 月</span>
@@ -37,7 +37,7 @@ export const PricingTable = () => {
                     <div className="flex justify-center absolute -top-3 inset-x-0">
                         <span className="h-6 flex justify-center items-center bg-indigo-500 text-white text-xs font-semibold tracking-widest uppercase rounded-full px-3 py-1">一番人気</span>
                     </div>
-                    <h3 className="text-gray-800 text-2xl font-semibold text-center">BUSINESS</h3>
+                    <h3 className="text-gray-800 text-2xl font-semibold text-center">ビジネス</h3>
                     <div className="flex justify-center items-end gap-1">
                         <span className="text-4xl text-gray-800 font-bold">¥8,000</span>
                         <span className="text-gray-500">/ 月</span>
@@ -49,7 +49,7 @@ export const PricingTable = () => {
 
                 <div className="bg-gray-100 rounded-lg space-y-3 px-6 py-6">
                     <div className="max-w-fit sm:mx-auto">
-                        <p className="text-gray-600 font-bold mb-4">STARTプランの機能に加え</p>
+                        <p className="text-gray-600 font-bold mb-4">スタンダードプランの機能に加え</p>
                         <Feature>サーバー容量 : 400GB</Feature>
                         <Feature>オリジナルデザインの実装</Feature>
                     </div>
@@ -60,7 +60,7 @@ export const PricingTable = () => {
             {/* <!-- plan - start --> */}
             <div className="flex flex-col space-y-4">
                 <div className="flex flex-col border rounded-lg gap-4 p-4 pt-6">
-                    <h3 className="text-gray-800 text-2xl font-semibold text-center">PREMIUM</h3>
+                    <h3 className="text-gray-800 text-2xl font-semibold text-center">プレミアム</h3>
                     <div className="flex justify-center items-end gap-1">
                         <span className="text-4xl text-gray-800 font-bold">¥10,000</span>
                         <span className="text-gray-500">/ 月</span>
@@ -72,7 +72,7 @@ export const PricingTable = () => {
 
                 <div className="bg-gray-100 rounded-lg space-y-3 px-6 py-6">
                     <div className="max-w-fit sm:mx-auto">
-                        <p className="text-gray-600 font-bold mb-4">BUSINESSプランの機能に加え</p>
+                        <p className="text-gray-600 font-bold mb-4">ビジネスプランの機能に加え</p>
                         <Feature>サーバー容量 : 無制限</Feature>
                         <Feature>オリジナル機能の実装</Feature>
                     </div>
