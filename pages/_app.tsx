833b2b0f082714e1464283f97a00f63a385df810
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-07 20:15:52 +0900

    Google Analytics用のGoogleタグの設置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import {AppProps} from "next/app"
+import Head from "next/head"
 import "../styles/index.css"
 
 export default function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <>
+                    {/* <!-- Google tag (gtag.js) --> */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V9833WVXT7"></script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-V9833WVXT7');
+                    `}} />
+                </>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
