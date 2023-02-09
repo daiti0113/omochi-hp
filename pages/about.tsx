8a02cbb01ef513c0407aef9658baeed7c94e054a
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-10 01:40:55 +0900

    余白を調整

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ export default function Index() {
                 </Head>
                 <SectionContainer>
                     <Profile />
-                    <p className="max-w-screen-md text-gray-600 text-sm md:text-base mt-20 text-center mx-auto">
+                    <p className="max-w-screen-md text-gray-600 text-sm md:text-base mt-10 md:mt-20 text-center mx-auto">
                         <span className="inline-block leading-relaxed">「最終的にはホームページ制作に限らず、</span>
                         <span className="inline-block leading-relaxed">ITのトータルサポートを目指しています。</span>
                         <span className="inline-block leading-relaxed">なにかお困りのことがありましたら、お気軽にご相談下さい。」</span>
