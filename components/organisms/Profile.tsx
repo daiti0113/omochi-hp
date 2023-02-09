d2b42ceeaa5fb9df8f547ebaca1ca059846c798b
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-10 01:40:21 +0900

    ソーシャルリンクを追加

--- components/organisms/Profile.tsx
+++ components/organisms/Profile.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import Link from "next/link"
+import {BsInstagram, BsLine, BsTwitter} from "react-icons/bs"
 import {Headline} from "../atoms/Headline"
 
 // eslint-disable-next-line max-lines-per-function
@@ -10,6 +12,19 @@ export const Profile = () => {
                     <div className="">
                         <Headline className="leading-9 text-gray-800 lg:text-left">砂川 大地</Headline>
                         <p className="font-normal text-sm mt-2 leading-6 text-gray-600 text-center lg:text-left">代表 / フロントエンドエンジニア</p>
+                        <div className="flex justify-center lg:justify-start mt-4">
+                            <div className="flex gap-4">
+                                <Link href="https://twitter.com/sunatti0113" target="_blank" className="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                    <BsTwitter size={24} />
+                                </Link>
+                                <Link href="https://lin.ee/QH64a9I" target="_blank" className="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                    <BsLine size={24}/>
+                                </Link>
+                                <Link href="https://www.instagram.com/daiti.s" target="_blank" className="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                                    <BsInstagram size={24}/>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                     <p className="font-normal text-sm md:text-base leading-6 text-gray-600 pt-10">
                         <span className="inline-block leading-relaxed">沖縄高専卒業後、就職のため上京し、株式会社KDDIウェブコミュニケーションズに入社。レンタルサーバー事業のフロントエンド開発を担当する。</span>
