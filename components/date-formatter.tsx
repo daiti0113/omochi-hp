18dcf4fa6d67f90f4a54ca3ca1e376a9c025e138
daiti0113 <stennis.ds.0113@gmail.com>
2023-02-18 17:02:48 +0900

    connpassのAPIがCORSエラーにならないようJSONPに変更 && スタイルを修正

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -3,11 +3,12 @@ import {zonedTimeToUtc} from "date-fns-tz"
 
 type Props = {
   dateString: string
+  showTime?: boolean
 }
 
-const DateFormatter = ({dateString}: Props) => {
+const DateFormatter = ({dateString, showTime}: Props) => {
     const date = zonedTimeToUtc(dateString, "Asia/Tokyo")
-    return <time dateTime={dateString}>{format(date, "yyyy-MM-dd")}</time>
+    return <time dateTime={dateString}>{format(date, showTime ? "yyyy-MM-dd HH:mm" : "yyyy-MM-dd")}</time>
 }
 
 export default DateFormatter
--- components/organisms/Events.tsx
+++ components/organisms/Events.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 import {useEffect, useState} from "react"
 import {Headline} from "../atoms/Headline"
 import {SectionContainer} from "../molecules/SectionContainer"
+import fetchJsonp from "fetch-jsonp"
+import DateFormatter from "../date-formatter"
 
 // eslint-disable-next-line max-lines-per-function
 export const Events = () => {
@@ -21,11 +23,12 @@ export const Events = () => {
                 <span className="inline-block leading-normal">沖縄県内のIT系の</span>
                 <span className="inline-block leading-normal">イベント情報をご紹介します。</span>
             </p>
-
-            {events.length > 0
-                ? events.map((event) => <Event event={event} key={event.event_id} />)
-                : <p className="text-center mx-auto pb-10">現在、イベント情報はありません</p>
-            }
+            <div className="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 md:gap-6 xl:gap-8">
+                {events.length > 0
+                    ? events.map((event) => <Event event={event} key={event.event_id} />)
+                    : <p className="text-center mx-auto pb-10">現在、イベント情報はありません</p>
+                }
+            </div>
         </SectionContainer>
     )
 }
@@ -49,7 +52,7 @@ type ConnpassResponse = {
 
 const getEvents = async () => {
     try {
-        const res = await fetch("https://connpass.com/api/v1/event/?keyword=%E6%B2%96%E7%B8%84&order=2&count=5")
+        const res = await fetchJsonp("https://connpass.com/api/v1/event/?keyword=%E6%B2%96%E7%B8%84&order=2&count=5")
         const connpassResponse = await res.json() as ConnpassResponse
         return connpassResponse.events
     } catch {
@@ -61,13 +64,13 @@ const Event = ({event}: {event: ConnpassResponse["events"][number]}) => {
     return (
         <div className="flex flex-col md:flex-row items-center border rounded-lg overflow-hidden">
             <div className="flex flex-col gap-2 p-4 lg:p-6">
-                <span className="text-gray-400 text-sm">{event.started_at}</span>
+                <span className="text-gray-400 text-sm"><DateFormatter dateString={event.started_at} showTime /></span>
 
                 <h2 className="text-gray-800 text-xl font-bold">
                     <a href="#" className="hover:text-indigo-500 active:text-indigo-600 transition duration-100">{event.title}</a>
                 </h2>
 
-                <p className="text-gray-500">{event.description}</p>
+                <p className="text-gray-500">{event.catch}</p>
 
                 <div>
                     <Link href={event.event_url} className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100">詳しく見る</Link>
