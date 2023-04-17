a6d02fc0e6d773c512c133136b6432788449153e
daiti0113 <stennis.ds.0113@gmail.com>
2023-04-18 01:05:23 +0900

    ヘッダーにロゴを追加

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 import {useRouter} from "next/router"
 import {useState} from "react"
@@ -12,11 +13,14 @@ export const Header = () => {
     return (
         <header className="flex justify-between items-center py-4 md:py-8 px-8 md:px-20 lg:px-44">
             {/* <!-- logo - start --> */}
-            <a href="/" className="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
-                <svg width="95" height="94" viewBox="0 0 95 94" className="w-6 h-auto text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-                </svg>
-                Omochi
+            <a href="/" aria-label="logo">
+                <Image
+                    loader={({src}) => src}
+                    src="/assets/logo.png"
+                    alt="プロフィール写真"
+                    width={230}
+                    height={45}
+                />
             </a>
             {/* <!-- logo - end --> */}
 
